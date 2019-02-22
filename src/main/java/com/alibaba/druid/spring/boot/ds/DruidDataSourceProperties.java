82a25fc81b1f49c2b75275a104aadd5d3ca00ea3
hnxyhcwdl1003@163.com <@vincent0506#>
2019-02-22 20:24:10 +0800

    修改

--- src/main/java/com/alibaba/druid/spring/boot/ds/DruidDataSourceProperties.java
+++ src/main/java/com/alibaba/druid/spring/boot/ds/DruidDataSourceProperties.java
@@ -43,14 +43,15 @@ public class DruidDataSourceProperties {
 	 * 配置间隔多久才进行一次检测，检测需要关闭的空闲连接，单位：毫秒;有两个含义：1) Destroy线程会检测连接的间隔时间 2)
 	 * testWhileIdle的判断依据，详细看testWhileIdle属性的说明
 	 */
-	private Long timeBetweenEvictionRunsMillis = 60000L;
+	private Long timeBetweenEvictionRunsMillis = DruidAbstractDataSource.DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS;
 	private Integer maxOpenPreparedStatements = -1;
 	private Integer numTestsPerEvictionRun = DruidAbstractDataSource.DEFAULT_NUM_TESTS_PER_EVICTION_RUN;
 
 	/** 超过时间限制是否回收 */
-	private boolean removeAbandoned = true;
+	private boolean removeAbandoned;
 	/** 超过时间限制多久触发回收逻辑，单位：毫秒 ，180000毫秒=3分钟 */
-	private Long removeAbandonedTimeoutMillis = 180 * 1000L;
+	private Long removeAbandonedTimeoutMillis = 300 * 1000L;
+    
 	private boolean logAbandoned;
 	private Integer connectionErrorRetryAttempts = 1;
 	private boolean breakAfterAcquireFailure = false;
@@ -58,25 +59,25 @@ public class DruidDataSourceProperties {
 	private boolean clearFiltersEnable;
 	private String connectProperties;
 	/** maxActive: 连接池最大连接数量 */
-	private Integer maxActive = 50;
+	private Integer maxActive = DruidAbstractDataSource.DEFAULT_MAX_ACTIVE_SIZE;
 	/**
 	 * 获取连接时最大等待时间，单位毫秒。配置了maxWait之后，缺省启用公平锁，并发效率会有所下降，如果需要可以通过配置useUnfairLock属性为true使用非公平锁。
 	 */
-	private Long maxWait = 60000L;
+	private Integer maxWait = DruidAbstractDataSource.DEFAULT_MAX_WAIT;
 	private Integer maxWaitThreadCount = -1;
 
 	private Integer maxIdle = DruidAbstractDataSource.DEFAULT_MAX_IDLE;
 	/** minIdle: 连接池最小连接数量 */
-	private Integer minIdle = 5;
+	private Integer minIdle = DruidAbstractDataSource.DEFAULT_MIN_IDLE;
 	/**
 	 * 要启用PSCache，必须配置大于0，当大于0时，poolPreparedStatements自动触发修改为true。在Druid中，不会存在Oracle下PSCache占用内存过多的问题，可以把这个数值配置大一些，比如说100
 	 */
-	private Integer maxPoolPreparedStatementPerConnectionSize = 20;
+	private Integer maxPoolPreparedStatementPerConnectionSize = 10;
 
 	/** 配置一个连接在池中最大生存的时间，单位:毫秒 */
-	private Long maxEvictableIdleTimeMillis;
+	private Long maxEvictableIdleTimeMillis = DruidAbstractDataSource.DEFAULT_MAX_EVICTABLE_IDLE_TIME_MILLIS;
 	/** 配置一个连接在池中最小生存的时间，单位：毫秒 */
-	private Long minEvictableIdleTimeMillis = 300000L;
+	private Long minEvictableIdleTimeMillis = DruidAbstractDataSource.DEFAULT_MIN_EVICTABLE_IDLE_TIME_MILLIS;
 
 	private Integer notFullTimeoutRetryCount = 0;
 
@@ -87,23 +88,23 @@ public class DruidDataSourceProperties {
 	 * 开启Druid的监控统计功能，mergeStat代替stat表示sql合并,wall表示防御SQL注入攻击
 	 */
 	private String filters = "mergeStat,wall,slf4j";
-	private Long phyTimeoutMillis;
+	private Long phyTimeoutMillis = DruidAbstractDataSource.DEFAULT_PHY_TIMEOUT_MILLIS;
 	private Long phyMaxUseCount = -1L;
 	/**
 	 * 是否缓存preparedStatement，也就是PSCache。PSCache对支持游标的数据库性能提升巨大，比如说oracle。在mysql5.5以下的版本中没有PSCache功能，建议关闭掉。5.5及以上版本有PSCache，建议开启。
 	 */
-	private boolean poolPreparedStatements = true;
+	private boolean poolPreparedStatements = false;
 	private boolean resetStatEnable;
 	private Integer statSqlMaxSize;
 	private boolean sharePreparedStatements = false;
 	/**
 	 * 建议配置为true，不影响性能，并且保证安全性。申请连接的时候检测，如果空闲时间大于timeBetweenEvictionRunsMillis，执行validationQuery检测连接是否有效。
 	 */
-	private boolean testWhileIdle = true;
+	private boolean testWhileIdle = DruidAbstractDataSource.DEFAULT_WHILE_IDLE;
 	/** 申请连接时执行validationQuery检测连接是否有效，做了这个配置会降低性能。 */
-	private boolean testOnBorrow = false;
+	private boolean testOnBorrow = DruidAbstractDataSource.DEFAULT_TEST_ON_BORROW;
 	/** 归还连接时执行validationQuery检测连接是否有效，做了这个配置会降低性能 */
-	private boolean testOnReturn = false;
+	private boolean testOnReturn = DruidAbstractDataSource.DEFAULT_TEST_ON_RETURN;
 	private Long timeBetweenConnectErrorMillis = DruidAbstractDataSource.DEFAULT_TIME_BETWEEN_CONNECT_ERROR_MILLIS;
 	private Long timeBetweenLogStatsMillis;
 	private Long transactionThresholdMillis = 0L;
@@ -113,7 +114,7 @@ public class DruidDataSourceProperties {
 	private boolean initGlobalVariants;
 	private boolean initVariants;
 	/** initialSize: 初始化时建立物理连接的个数。初始化发生在显示调用init方法，或者第一次getConnection时 */
-	private Integer initialSize = 15;
+	private Integer initialSize = DruidAbstractDataSource.DEFAULT_INITIAL_SIZE;
 	private boolean useUnfairLock;
 	private boolean useGlobalDataSourceStat;
 	/**
@@ -266,11 +267,11 @@ public class DruidDataSourceProperties {
 		this.maxActive = maxActive;
 	}
 
-	public Long getMaxWait() {
+	public Integer getMaxWait() {
 		return maxWait;
 	}
 
-	public void setMaxWait(Long maxWait) {
+	public void setMaxWait(Integer maxWait) {
 		this.maxWait = maxWait;
 	}
 
